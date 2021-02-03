6af198604ff95a71b53f4e90d65e8d5be792f659
今野郷 <xiangjinye1@gmail.com>
2021-02-03 22:58:20 +0900

    勉強

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,17 @@
+class HelloController < ApplicationController
+    def index
+        render plain: 'こんにちは、世界！'
+    end
+
+    def view
+        @msg = 'こんにちは、世界！'
+    end
+
+    def list
+        @books = Book.all
+    end
+
+    def app_var
+        render plain: MY_APP['logo']['source']
+    end
+end
